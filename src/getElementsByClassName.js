dc50a43b6d7698f4e1a10cff8ef4bf65b1e7574f
NicholasBelow <nkbelow@gmail.com>
2017-01-26 16:28:14 -0800

    Fix function so that test passes
    Use underscore each and contains functions to check if the particular
    node contains the desired class. Then use each to check if any of the
    childnode has the desired class in a classlist

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var classHolder = [];
+  function checkForClass(node) {
+  	//use underscore contains to check if classname is in classlist
+    if (_.contains(node.classList,(className))) {
+      classHolder.push(node);
+    }
+    //use each to check if any of the child nodes have the className
+    _.each(node.childNodes, function(childNode) {
+    	checkForClass(childNode); //use recursion to check if className is present for each node
+    });
+   }
+   checkForClass(document.body);
+    return classHolder;
 };
